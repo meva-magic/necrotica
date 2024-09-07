cba48a7fae972746d2ddf8919d57dea4e6e464d2
saharov-dev <saharov.dev@gmail.com>
2024-09-07 13:08:02 +0300

    :3

--- NECROTICA/Assets/Scripts/Enemy/AngleToPlayer.cs
+++ NECROTICA/Assets/Scripts/Enemy/AngleToPlayer.cs
@@ -8,7 +8,11 @@ public class AngleToPlayer : MonoBehaviour
     private Vector3 targetPosition;
     private Vector3 targetDirection;
 
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite[] sprites;
+
     [SerializeField] private float angle;
+    [SerializeField] private int lastIndex;
 
 
     private void Start()
@@ -22,6 +26,49 @@ public class AngleToPlayer : MonoBehaviour
         targetDirection = targetPosition - transform.position;
 
         angle = Vector3.SignedAngle(targetDirection, transform.forward, Vector3.up);
+
+        Vector3 tempScale = Vector3.one;
+        if(angle > 0)
+        {
+            tempScale.x *= -1f;
+        }
+
+        spriteRenderer.transform.localScale = tempScale;
+
+        lastIndex = GetIndex(angle);
+
+        spriteRenderer.sprite = sprites[lastIndex];
+    }
+
+    private int GetIndex(float angle)
+    {
+        if(angle > -22.5f && angle < 22.6f)
+        {return 0;}
+
+        if(angle >= 22.5f && angle < 67.5f)
+        {return 7;}
+
+        if(angle >= 67.5f && angle < 112.5f)
+        {return 6;}
+
+        if(angle >= 112.5f && angle >=157.5f)
+        {return 5;}
+
+
+
+        if(angle >= -157.5f && angle >= 157.5f)
+        {return 4;}
+
+        if(angle >= -157.4f && angle < -112.5f)
+        {return 3;}
+        
+        if(angle >= -112.5f && angle < -67.5f)
+        {return 2;}
+
+        if(angle >= -67.5f && angle <= -22.5f)
+        {return 1;}
+
+        return lastIndex;
     }
 
     void OnGizmosSelected()
